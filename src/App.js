d6376dbb7da192aa2cc8fdd28a7baf1078b97d1d
phankhai04112004 <pkhai7444@gmail.com>
2025-06-26 16:25:23 +0700

    Sửa lại phần About

--- src/App.js
+++ src/App.js
@@ -1,31 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-<<<<<<< khai/update/about
 import About from "./pages/About/About"
-=======
-import About from "./pages/About";
->>>>>>> master
 import './index.css';
 import Home from "./pages/Home";
 
 
 function App() {
   return (
-<<<<<<< khai/update/about
     <div className="min-h-screen bg-black text-white max-w-screen-2xl">
       <Navbar />
       <Routes>
         <Route path="/about" element={<About />} />
       </Routes>
-=======
-    <div className="flex flex-col min-h-screen bg-black text-white">
-      <Navbar />
       <Home />
->>>>>>> master
       <Footer />
     </div>
   );
 }
-
 export default App;
