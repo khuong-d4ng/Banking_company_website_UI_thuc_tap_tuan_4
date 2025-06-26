2c54cb07883edccce8fdbb4a48eb2273453908ce
khuong-d4ng <khuongdang.pham@gmail.com>
2025-06-26 16:34:46 +0700

    Chỉnh sửa App.js cho homepage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import About from "./pages/About";
 import './index.css';
 import Home from "./pages/Home";
 
-
 function App() {
   return (
-    <div className="flex flex-col min-h-screen bg-black text-white">
+    <div className="flex flex-col min-h-screen bg-black text-white max-w-screen-2xl">
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
       <Footer />
     </div>
   );
